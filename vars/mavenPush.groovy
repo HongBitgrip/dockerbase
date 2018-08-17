d04ebf1eeebdd56f8c5e2336d9cebc066134ba6b
Hong Nguyen <hong.nguyen@bitgrip.de>
2018-08-17 17:49:20 +0200

    change port for nexus

--- vars/mavenPush.groovy
+++ vars/mavenPush.groovy
@@ -1,5 +1,5 @@
 def call(modulePath, fileName, version) {
   def repository = "docker-private"
-  def url = "http://5.189.132.250:8083"
+  def url = "http://5.189.132.250:8081"
   sh "mvn deploy:deploy-file -q -Dfile=${modulePath}/target/${fileName} -DpomFile=${modulePath}/pom.xml -DgeneratePom=false -DrepositoryId=${repository} -Durl=${url} -Dversion=${version}"
 }
