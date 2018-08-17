ac19f65cb50885953f8f3e39d1f4ed30ebf29c8b
Hong Nguyen <hong.nguyen@bitgrip.de>
2018-08-17 13:45:21 +0200

    add maven push

--- vars/mavenPush.groovy
+++ vars/mavenPush.groovy
@@ -1,3 +1,5 @@
-def call() {
-  sh "echo Test Groovy Pipeline"  
+def call(modulePath, fileName, version) {
+  def repository = "docker-private"
+  def url = "http://5.189.132.250:8083"
+  sh "mvn deploy:deploy-file -q -Dfile=${modulePath}/target/${fileName} -DpomFile=${modulePath}/pom.xml -DgeneratePom=false -DrepositoryId=${repository} -Durl=${url} -Dversion=${version}"
 }
